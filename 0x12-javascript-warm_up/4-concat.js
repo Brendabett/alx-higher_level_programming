d31c037f258f0a0b26e13ddd788119243b91efbe
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-05 19:39:08 +0300

    My commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
-if (Number.isNaN(number)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + number);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
