68961cc7b17eef01a761569cc6c159cfde381567
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-05 19:44:06 +0300

    My commit

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const { argv } = require('process');
-const a = Number(argv[2]);
-const b = Number(argv[3]);
-const add = (a, b) => a + b;
-console.log(add(a, b));
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
+function add (a, b) {
+  console.log(a + b);
+}
+add(a, b);
