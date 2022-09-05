383f97fac689bc3e00e476cfaf306ed175a914a0
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-05 19:37:30 +0300

    My commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
-// script that prints two arguments passed to it, in the following format: “ is ”
-const { argv } = require('process');
-let first;
-let second;
-
-if (argv[2] === undefined) {
-  first = 'undefined';
+const number = parseInt(process.argv[2]);
+if (Number.isNaN(number)) {
+  console.log('Not a number');
 } else {
-  first = argv[2];
+  console.log('My number: ' + number);
 }
-if (argv[3] === undefined) {
-  second = 'undefined';
-} else {
-  second = argv[3];
-}
-console.log(`${first} is ${second}`);
