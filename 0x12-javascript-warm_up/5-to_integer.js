94c7f84b52b464733e01104754d34c7974ea83c3
Brendabett <brendabett33@gmail.com>
2022-09-05 10:03:26 -0700

    My commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,3 +5,9 @@ if (Number.isNaN(number)) {
 } else {
   console.log('My number: ' + number);
 }
+const number = parseInt(process.argv[2]);
+if (Number.isNaN(number)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + number);
+}
