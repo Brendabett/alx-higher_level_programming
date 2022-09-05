615b8bdd86064cd572a7dece1fd868f5bd5be903
Brendabett <brendabett33@gmail.com>
2022-09-05 10:23:15 -0700

    My commit

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-console.log(process.argv[2] + ' is ' + process.argv[3]);
-
-console.log(process.argv[2] + ' is ' + process.argv[3]);
-=======
->>>>>>> acf0e3505826d7e8911407a1675f1e13e9698597
+console.log(process.argv[2] +  is  + process.argv[3]);
