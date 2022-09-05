0018077c4dd450d78957ceca566aadd543c4887a
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-09-05 20:10:49 +0300

    My commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-<<<<<<< HEAD
 const number = parseInt(process.argv[2]);
 if (Number.isNaN(number)) {
   console.log('Not a number');
@@ -12,5 +11,3 @@ if (Number.isNaN(number)) {
 } else {
   console.log('My number: ' + number);
 }
-=======
->>>>>>> acf0e3505826d7e8911407a1675f1e13e9698597
