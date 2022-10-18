38121c052f7cb320dca3170e5fc883d21e90eb42
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-18 13:37:24 +0300

    My commit

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,6 +2,7 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
+  if (!error) {
     const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
@@ -9,6 +10,7 @@ request(url, function (error, response, body) {
 
 function printCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
+    if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
         printCharacters(characters, index + 1);
