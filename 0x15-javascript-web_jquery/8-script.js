578d296b6dfef4175d17dbfa6a23528d3ea324f7
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-26 14:33:39 +0300

    My commit

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,3 +1,3 @@
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  .append(...data.results.map(movie => ));
+  $('UL#list_movies').append(...data.results.map(movie => `<li>${movie.title}</li>`));
 });
