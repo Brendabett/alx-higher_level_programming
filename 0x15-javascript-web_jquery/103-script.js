9901de9a1d50f166b077e38cd1c825820fbf5705
Brendabett <98813715+Brendabett@users.noreply.github.com>
2022-10-26 14:49:42 +0300

    My commit

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,7 +1,7 @@
-.ready(function () {
-  .click(translate);
-  .focus(function () {
-    .keydown(function (e) {
+$('document').ready(function () {
+  $('INPUT#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
       if (e.keyCode === 13) {
         translate();
       }
@@ -11,7 +11,7 @@
 
 function translate () {
   const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $.get(url + $.param({ lang: .val() }), function (data) {
-    .html(data.hello);
+  $.get(url + $.param({ lang: $('INPUT#language_code).val() }), function (data) {
+    $('DIV#hello').html(data.hello);
   });
 }
